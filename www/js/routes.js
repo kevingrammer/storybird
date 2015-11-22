a3c139b6f3ab9ca24b4f1d6706941a90df5c9753
Kevin Grammer <kevingrammer@gmail.com>
2015-11-22 16:34:35 -0500

    style: Clean up formatting

--- www/js/routes.js
+++ www/js/routes.js
@@ -2,34 +2,20 @@ angular.module('app.routes', [])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+  // Routes
   $stateProvider
-    
-      
-        
     .state('storybird', {
       url: '/home',
       templateUrl: 'templates/storybird.html',
       controller: 'storybirdCtrl'
     })
-        
-      
-    
-      
-        
     .state('newFlight', {
       url: '/newflight',
       templateUrl: 'templates/newFlight.html',
       controller: 'newFlightCtrl'
     })
-        
-      
     ;
-
-  // if none of the above states are matched, use this as the fallback
+  // Default route
   $urlRouterProvider.otherwise('/home');
 
 });
