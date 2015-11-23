b26bea8d80bb0da8e5c934aca1f813912a0e8b9f
Kevin Grammer <kevingrammer@gmail.com>
2015-11-22 21:45:22 -0500

    feat: Create sb-egg directive

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,16 +1,15 @@
 angular.module('app.directives', [])
-
-.directive('sbEgg', [function(){
-  return {
-    templateUrl: 'templates/sb-egg.html',
-    restrict: 'AE',
-    transclude: true,
-    scope: {
-      eggContent: '=sbEggContent'
-    },
-    link: function postLink(scope. element, attrs) {
-      scope.eggContent = scope.eggContent + '!';
+  .directive('sbEgg', function() {
+    return {
+      templateUrl: 'templates/sb-egg.html',
+      restrict: 'AE',
+      transclude: true,
+      scope: {
+        eggTitle: '=sbEggTitle',
+        eggContent: '=sbEggContent'
+      },
+      link: function postLink(scope, element, attrs) {
+      }
     }
-  }
-}]);
-
+  })
+  ;
